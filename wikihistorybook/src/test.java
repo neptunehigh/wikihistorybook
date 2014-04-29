214b687fb28a3c37a1c6c20af6351c015ac6d910
Simon Flühmann <simon.fluehmann@students.fhnw.ch>
2014-04-29 16:09:52 +0200

    initital eclipse commit

---
+++ wikihistorybook/src/test.java
@@ -0,0 +1,17 @@
+
+public class test {
+
+	/**
+	 * Testclass
+	 * @author simon flühmann
+	 */
+	
+	
+	// main method
+	public static void main(String[] args) {
+
+		System.out.println("Hello world");
+		
+	}
+
+}
