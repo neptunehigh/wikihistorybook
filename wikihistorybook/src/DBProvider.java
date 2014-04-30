1b0e686819f6af41fb057703d18179177b3f0c5c
yuliaschmitt <yulia.schmitt@students.fhnw.ch>
2014-04-30 18:39:01 +0200

    DB Connection implemented

---
+++ wikihistorybook/src/DBProvider.java
@@ -0,0 +1,74 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBProvider {
+
+	//singleton
+	private DBProvider() {}
+	
+	private static DBProvider provider = null;
+	private static Connection connection;
+	private Statement statement;
+	private final String CONN_QUERY= "jdbc:mysql://91.250.82.104:3306/wikihistory?user=wikipulse&password=COINcourse2014";
+
+	public static DBProvider getInstance() {
+		if (provider == null)
+			provider = new DBProvider();
+		return provider;
+	}
+	
+	public Connection getConnection()
+	{
+		if(connection==null)		
+			try {
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+				connection = DriverManager
+						.getConnection(CONN_QUERY);
+			} catch (ClassNotFoundException e) {
+				System.out.println("Class not found!");
+			} catch (SQLException e) {
+				System.out.println("SQL Exception!");
+			} catch (Exception ex) {
+				System.out.println("Exception");
+			}
+		return connection;		
+	}
+	
+	public void closeConnection()
+	{
+		try {
+			if (connection!=null && !connection.isClosed())
+			{
+				connection.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Führt mysql select query aus
+	 * 
+	 * @param query
+	 *            String Query, die ausgeführt werden soll
+	 * @return ResultSet oder null
+	 */
+	public ResultSet executeQuery(String query){
+		ResultSet res=null;
+		if(connection==null)getConnection();
+		
+		try {
+			statement = connection.createStatement();
+			res = statement.executeQuery(query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return res;
+	}
+
+
+}
--- wikihistorybook/src/test.java
+++ wikihistorybook/src/test.java
@@ -1,3 +1,6 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 
 public class test {
 
@@ -5,13 +8,33 @@ public class test {
 	 * Testclass
 	 * @author simon flŸhmann
 	 */
-	
-	
+		
 	// main method
 	public static void main(String[] args) {
-
-		System.out.println("Hello world");
 		
+		//DB als Singleton
+		DBProvider db = DBProvider.getInstance();		
+		//DB Verbindung öffnen
+		db.getConnection();
+		
+		//SQL Query als String formulieren
+		String query="SELECT * FROM wikihistory.people where year_from > 1964";
+		//Query ausführen
+		ResultSet result= db.executeQuery(query);
+		
+		
+		//Resultat ausgeben
+		try {
+			while ( result.next() )
+				  System.out.printf( "name: %s, year_from: %s%n", result.getString(2),
+						  result.getString(5));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		//DB Verbindung schliessen
+		db.closeConnection();		
 	}
 
 }
