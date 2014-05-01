55463c756c7d7263c6c1bf989a854833ebffcfd3
delta99 <silvioPi>
2014-05-01 18:01:50 +0200

    Table size time measuring

--- wikihistorybook/src/dbTest/test.java
+++ wikihistorybook/src/dbTest/test.java
@@ -14,17 +14,41 @@ public class test {
 	// main method
 	public static void main(String[] args) {
 		
+		 long startTime = System.currentTimeMillis();
+		
 		//DB als Singleton
 		DBProvider db = DBProvider.getInstance();		
 		//DB Verbindung ?ffnen
 		db.getConnection();
 		
+		System.out.println("connected. ("+(System.currentTimeMillis()-startTime)+" ms)");
+		System.out.println();
+		
+		String query1 = "SELECT COUNT(*) FROM wikihistory.people";
+		ResultSet result1= db.executeQuery(query1);
+		try {
+			result1.first();
+			System.out.println("Table Size: "+result1.getString(1));
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		String query3="SELECT COUNT(*) FROM wikihistory.people where year_from > 1964";
+		ResultSet result3= db.executeQuery(query3);
+		try {
+			result3.first();
+			System.out.println("Result Size: "+result3.getString(1));
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		System.out.println("processing....");
 		//SQL Query als String formulieren
-		String query="SELECT * FROM wikihistory.people where year_from > 1964";
+		String query2="SELECT * FROM wikihistory.people where year_from > 1964";
 		//Query ausf?hren
-		ResultSet result= db.executeQuery(query);
-		
-		
+		ResultSet result= db.executeQuery(query2);
 		//Resultat ausgeben
 		try {
 			while ( result.next() )
@@ -33,7 +57,7 @@ public class test {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+		System.out.println("finished. ("+(System.currentTimeMillis()-startTime)+" ms)");
 		
 		//DB Verbindung schliessen
 		db.closeConnection();		
