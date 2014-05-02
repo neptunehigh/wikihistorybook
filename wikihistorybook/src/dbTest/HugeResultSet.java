2272c3b7a5a67f343cead7c8a99bf7e41c27131a
delta99 <silvioPi>
2014-05-02 15:35:43 +0200

    HugeResultSet better performance

---
+++ wikihistorybook/src/dbTest/HugeResultSet.java
@@ -0,0 +1,55 @@
+package dbTest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Set;
+
+public class HugeResultSet {
+
+  public static void main(String [] args) {
+	  long start = System.currentTimeMillis();
+	  HashMap<Integer, String> people = new HashMap<Integer, String>();
+	  Connection connection = null;
+      String url = "jdbc:mysql://91.250.82.104:3306/";
+      String dbName = "wikihistory";
+      String driverName = "com.mysql.jdbc.Driver";
+      String userName = "wikipulse";
+      String password = "COINcourse2014";
+      
+      
+      try {
+        Class.forName(driverName).newInstance();
+        connection = DriverManager.getConnection(url+dbName, userName, password);
+        PreparedStatement  stmt = connection.prepareStatement("SELECT id, name FROM wikihistory.people where year_from > ?");
+        stmt.setInt(1, 1964);
+        ResultSet res = stmt.executeQuery();
+        if (res.getType() == ResultSet.TYPE_FORWARD_ONLY) {
+          System.out.println("ResultSet Type Forword-Only.");
+        } else {
+          System.out.println("ResultSet scrollable.");
+        }
+        res.first();
+       
+        while (res.next()){
+        	people.put(res.getInt("id"), res.getString("name"));
+        }
+        res.close();
+        stmt.close();
+        
+        connection.close();
+      } catch (Exception e) {
+          System.err.println("Exception: "+ e.getMessage());
+      }
+      Set<Integer> keys = people.keySet();
+      for(Integer key: keys){
+          System.out.println("Value of "+key+" is: "+people.get(key));
+      }
+      System.out.println(people.size());
+      System.out.println("finished in : "+(System.currentTimeMillis()-start));
+    }
+}
--- wikihistorybook/src/dbTest/test.java
+++ wikihistorybook/src/dbTest/test.java
@@ -5,51 +5,28 @@ import java.sql.SQLException;
 
 
 public class test {
+	
 
 	/**
 	 * Testclass
 	 * @author simon fl?hmann
 	 */
-		
+
 	// main method
 	public static void main(String[] args) {
-		
-		 long startTime = System.currentTimeMillis();
-		
+		  long start = System.currentTimeMillis();
+
 		//DB als Singleton
 		DBProvider db = DBProvider.getInstance();		
 		//DB Verbindung ?ffnen
 		db.getConnection();
-		
-		System.out.println("connected. ("+(System.currentTimeMillis()-startTime)+" ms)");
-		System.out.println();
-		
-		String query1 = "SELECT COUNT(*) FROM wikihistory.people";
-		ResultSet result1= db.executeQuery(query1);
-		try {
-			result1.first();
-			System.out.println("Table Size: "+result1.getString(1));
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		String query3="SELECT COUNT(*) FROM wikihistory.people where year_from > 1964";
-		ResultSet result3= db.executeQuery(query3);
-		try {
-			result3.first();
-			System.out.println("Result Size: "+result3.getString(1));
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		System.out.println("processing....");
+
 		//SQL Query als String formulieren
-		String query2="SELECT * FROM wikihistory.people "
-				+ "WHERE year_from > 1964";
+		String query="SELECT * FROM wikihistory.people where year_from > 1964";
 		//Query ausf?hren
-		ResultSet result= db.executeQuery(query2);
+		ResultSet result= db.executeQuery(query);
+
+
 		//Resultat ausgeben
 		try {
 			while ( result.next() )
@@ -58,10 +35,12 @@ public class test {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		System.out.println("finished. ("+(System.currentTimeMillis()-startTime)+" ms)");
-		
+
+
 		//DB Verbindung schliessen
 		db.closeConnection();		
+	    System.out.println("finished in : "+(System.currentTimeMillis()-start));
+
 	}
 
 }
