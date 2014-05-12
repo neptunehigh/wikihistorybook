b8d553da2ce17626b066961a1a1ab058e63a0b53
revolucerne <che-sid@bluewin.ch>
2014-05-12 22:27:49 +0200

    Clean code
    Cleaned code from unnecessary lines.

--- wikihistorybook/src/textInput/TextfileRegexSimpleConsole.java
+++ wikihistorybook/src/textInput/TextfileRegexSimpleConsole.java
@@ -3,10 +3,8 @@ package textInput;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
