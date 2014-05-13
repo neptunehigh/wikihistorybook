d7137b2bd9a73ae0deca0c89410e9374fde7a59f
delta99 <silvioPi>
2014-05-13 21:33:00 +0200

    improved

--- wikihistorybook/src/wikiBook/InternalMouseManager.java
+++ wikihistorybook/src/wikiBook/InternalMouseManager.java
@@ -13,36 +13,26 @@ public class InternalMouseManager extends DefaultMouseManager {
 	GraphicElement ge = null;
 	
 	public InternalMouseManager(GraphicElement e){
-//		super();
+		super();
 		this.ge = e;
 	}
 	
 	public InternalMouseManager(){
-//		super();
-	}
-	
-	protected void mouseButtonPressOnElement(GraphicElement element,
-			MouseEvent event) {
-		super.mouseButtonPressOnElement(element, event);
-
-		JOptionPane.showMessageDialog(view, element.getLabel());
-	}
-	
-	public void mouseClicked(MouseEvent e){
-		
-	}
-	
-	public void mouseDragged(MouseEvent e){
-		
+		super();
 	}
 	
 	public void mousePressed(MouseEvent e){
-		
+		ArrayList<GraphicElement> a = view.allNodesOrSpritesIn(e.getX()-1, e.getY()-1, e.getX()+1, e.getY()+1);
+		if(!a.isEmpty()){
+			JOptionPane.showMessageDialog(view,
+					"You click on " + a.get(0).getLabel());
+		}
 	}
+	
 	
 	public void mouseMoved(MouseEvent e){
 		super.mouseMoved(e);
-		ArrayList<GraphicElement> a = view.allNodesOrSpritesIn(e.getX()-6, e.getY()-6, e.getX()+6, e.getY()+6);
+		ArrayList<GraphicElement> a = view.allNodesOrSpritesIn(e.getX()-2, e.getY()-2, e.getX()+2, e.getY()+2);
 		if(!a.isEmpty()){
 			if(ge!= null){
 				ge.changeAttribute("ui.style", "text-mode: hidden;");
