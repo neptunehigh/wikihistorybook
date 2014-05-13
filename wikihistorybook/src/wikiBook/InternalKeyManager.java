dfe5796a8adff9e526a042875fd0fe1ecb03a8b1
delta99 <silvioPi>
2014-05-13 22:35:52 +0200

    tried to fix

--- wikihistorybook/src/wikiBook/InternalKeyManager.java
+++ wikihistorybook/src/wikiBook/InternalKeyManager.java
@@ -5,8 +5,9 @@ import java.awt.event.KeyEvent;
 import org.graphstream.ui.swingViewer.util.DefaultShortcutManager;
 
 public class InternalKeyManager extends DefaultShortcutManager {
-	public void keyTyped(KeyEvent e) {
-		// Invoked when a key has been typed.
+	
+	public InternalKeyManager(){
+		super();
 	}
 
 	public void keyPressed(KeyEvent e) {
@@ -16,16 +17,15 @@ public class InternalKeyManager extends DefaultShortcutManager {
 
 		case KeyEvent.VK_A:
 			view.getCamera().setViewPercent(p + 0.1);
+			System.out.println("s");
 			break;
 
 		case KeyEvent.VK_Q:
 			view.getCamera().setViewPercent(p - 0.1);
 			break;
-
+		default:
+			System.out.println("keypressed");
 		}
 	}
 
-	public void keyReleased(KeyEvent e) {
-		// Invoked when a key has been released.
-	}
 }
